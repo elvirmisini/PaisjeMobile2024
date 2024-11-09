5b8321510495d9f1898fe5b5b2b62a1c7e6c7724
elvirmisinishopaz <elvirmisini@gmail.com>
2024-11-09 17:56:35 +0100

    login form validation v1

--- app/src/main/java/com/example/g1/LoginMainActivity.java
+++ app/src/main/java/com/example/g1/LoginMainActivity.java
@@ -1,6 +1,11 @@
 package com.example.g1;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +17,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginMainActivity extends AppCompatActivity {
 
-
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +27,31 @@ public class LoginMainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_main);
 
+        emailEditText=findViewById(R.id.editTextTextEmailAddress);
+        passwordEditText=findViewById(R.id.editTextTextPassword);
+        loginButton=findViewById(R.id.button);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        loginButton.setOnClickListener(view->validateFields());
+    }
+
+    private void validateFields(){
+        String email=emailEditText.getText().toString();
+        String password=passwordEditText.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter your email!",Toast.LENGTH_SHORT).show();
+        } else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this,"Please enter a valid email address!",Toast.LENGTH_SHORT).show();
+        }else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this,"Please enter your password!",Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this,"Success!",Toast.LENGTH_SHORT).show();
+        }
     }
 }
